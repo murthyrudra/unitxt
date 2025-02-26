2a0e59715ccbbf1ca076835f5b41f28ec9263987
Rudra Murthy <rudramurthy.iisc@gmail.com>
2025-02-25 23:14:30 -0500

    Token authentication set to true

--- src/unitxt/loaders.py
+++ src/unitxt/loaders.py
@@ -296,7 +296,7 @@ class LoadHF(LazyLoader):
                         download_config=DownloadConfig(
                             max_retries=settings.loaders_max_retries,
                             # extract_on_the_fly=True,
-                        ),
+                        ),token=True,
                     )
                 except ValueError as e:
                     if "trust_remote_code" in str(e):
